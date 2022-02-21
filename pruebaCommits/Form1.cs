d774ebe1a2ffb333dd8b18d70e896783a402c178
alexescobar28 <alex.escobar@epn.edu.ec>
2022-02-21 00:21:11 -0500

    Agregar archivos de proyecto.

---
+++ pruebaCommits/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaCommits
+{
+    public partial class Form1 : Form
+    {
+        String nombre;
+        String curso;
+        String apellido;
+        String sexo;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nombre = textBox1.Text;
+            curso = textBox2.Text;
+            apellido = textBox3.Text;
+            sexo = comboBox1.Text;
+            label6.Text = nombre + curso + apellido + sexo;
+        }
+    }
+}
